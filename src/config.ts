c975e9cfabbe8efcfad30c22e512281749c9c96d
Denis <carriere.denis@gmail.com>
2019-01-25 10:20:44 -0500

    Replace config style

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import Axios from "axios"
+
+export class config {
+  public static token: string;
+  public static endpoint: string = "https://api.eosdetective.semiofficial.io/v0/";
+
+  set token(token: string) {
+    this.token = token;
+    Axios.defaults.headers.common["X-Api-Key"] = token
+  }
+  get token() {
+    return this.token;
+  }
+  set endpoint(endpoint: string) {
+    this.endpoint = endpoint;
+    Axios.defaults.baseURL = endpoint
+  }
+  get endpoint() {
+    return this.endpoint;
+  }
+}
