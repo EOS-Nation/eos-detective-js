afeceda1ef6940fec4c2bec205ee169863d62b54
Denis <carriere.denis@gmail.com>
2019-01-30 13:40:47 -0500

    Update profile types

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,17 +1,37 @@
 export interface Category {
-  percentage: number
-  volume: number
-  count: number
+    percentage: number
+    volume: number
+    count: number
 }
 
 export interface Profile {
+  /**
+   * Account name
+   */
   account: string,
+  /**
+   * @example "eosio.token"
+   */
   contract: string,
+  /**
+   * @example "EOS"
+   */
   token: string,
+  /**
+   * @example "eos"
+   */
+  chain: string,
   total_tokens_received: number;
   total_incoming_transfers: number;
+  /**
+   * @example 0 = fail, 70 < fail, 70+ pass
+   */
   score: number;
+  /**
+   * @example "blacklist" / "exchange" / "genesis"
+   */
+  type: string | null;
   categories: {
-    [category: string]: Category
+      [category: string]: Category
   }
 }
