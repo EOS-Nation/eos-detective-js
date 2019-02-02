c43f65357ed0c41b23c137998bfce57c0e736e4f
Denis <carriere.denis@gmail.com>
2019-02-02 13:43:08 -0500

    Remove default excludes

--- src/api/transfers.ts
+++ src/api/transfers.ts
@@ -46,7 +46,7 @@ export async function transfers(
     /**
      * Accounts to exclude from query.
      *
-     * @default `["eosio.stake", "eosio.ram", "eosio.ramfee"]`
+     * @default `[]`
      */
     excludes?: string[]
     /**
@@ -95,29 +95,17 @@ export async function transfers(
   queryParams.symbol = options.symbol || "EOS"
   queryParams.direction = options.direction || "both"
   queryParams.accumulated = options.accumulated === undefined ? true : options.accumulated
-  queryParams.excludes = options.excludes || ["eosio.ram", "eosio.ramfee", "eosio.stake"]
+  queryParams.excludes = options.excludes || []
   queryParams.quantity_min = options.quantity_min === undefined ? 1000 : Math.round(options.quantity_min * 10000)
   queryParams.time_min = options.time_min === undefined ? yesterday.toISOString() : options.time_min.toISOString()
 
   // optional params
-  if (options.quantity_max) {
-    queryParams.quantity_max = Math.round(options.quantity_max * 10000)
-  }
-  if (options.time_max) {
-    queryParams.time_max = options.time_max.toISOString()
-  }
-  if (options.accumulated_min) {
-    queryParams.accumulated_min = Math.round(options.accumulated_min * 10000)
-  }
-  if (options.accumulated_max) {
-    queryParams.accumulated_max = Math.round(options.accumulated_max * 10000)
-  }
-  if (options.count_min) {
-    queryParams.count_min = options.count_min
-  }
-  if (options.count_max) {
-    queryParams.count_max = options.count_max
-  }
+  if (options.quantity_max) { queryParams.quantity_max = Math.round(options.quantity_max * 10000) }
+  if (options.time_max) { queryParams.time_max = options.time_max.toISOString() }
+  if (options.accumulated_min) { queryParams.accumulated_min = Math.round(options.accumulated_min * 10000) }
+  if (options.accumulated_max) { queryParams.accumulated_max = Math.round(options.accumulated_max * 10000) }
+  if (options.count_min) { queryParams.count_min = options.count_min }
+  if (options.count_max) { queryParams.count_max = options.count_max }
 
   try {
     const res = await Axios.request<ApiResponse<TransfersData>>({
