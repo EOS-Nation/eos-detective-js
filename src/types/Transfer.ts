45e185302e93ee550f1cc71bd0e124cf5f645fa1
Jan Langheimer <langheimer@gmail.com>
2020-08-26 15:35:36 +0200

    add multichain precision support

--- src/types/Transfer.ts
+++ src/types/Transfer.ts
@@ -1,20 +1,20 @@
-import { Account } from "./Account"
-import { preciseRound } from "../utils"
+import { Account } from "./Account";
+import { convertQuantity } from "../utils";
 
-export type Direction = "incoming" |"outgoing" | "both" | "between";
+export type Direction = "incoming" | "outgoing" | "both" | "between";
 
 export class Transfer {
-  private readonly _from: Account
-  private readonly _to: Account
-  private readonly _quantity: number
-  private readonly _symbol: string
-  private readonly _chain: string
-  private readonly _contract: string
-  private readonly _time?: Date
-  private readonly _blockNum?: number
-  private readonly _count?: number
-  private readonly _trxId?: string
-  private readonly _memo?: string
+  private readonly _from: Account;
+  private readonly _to: Account;
+  private readonly _quantity: number;
+  private readonly _symbol: string;
+  private readonly _chain: string;
+  private readonly _contract: string;
+  private readonly _time?: Date;
+  private readonly _blockNum?: number;
+  private readonly _count?: number;
+  private readonly _trxId?: string;
+  private readonly _memo?: string;
 
   public constructor(
     from: string,
@@ -29,17 +29,17 @@ export class Transfer {
     trxId?: string,
     memo?: string
   ) {
-    this._from = new Account(from)
-    this._to = new Account(to)
-    this._quantity = preciseRound(quantity / 10000, 4)
-    this._symbol = symbol
-    this._chain = chain
-    this._contract = contract
-    this._time = time
-    this._blockNum = blockNum
-    this._count = count
-    this._trxId = trxId
-    this._memo = memo
+    this._from = new Account(from);
+    this._to = new Account(to);
+    this._quantity = convertQuantity(quantity, chain);
+    this._symbol = symbol;
+    this._chain = chain;
+    this._contract = contract;
+    this._time = time;
+    this._blockNum = blockNum;
+    this._count = count;
+    this._trxId = trxId;
+    this._memo = memo;
   }
 
   public static fromJson(value: any): Transfer {
@@ -55,11 +55,11 @@ export class Transfer {
       value!.count,
       value!.trx_id,
       value!.memo
-    )
+    );
   }
 
   public toString(): string {
-    return JSON.stringify(this.toJson)
+    return JSON.stringify(this.toJson);
   }
 
   public toJson(): any {
@@ -74,91 +74,91 @@ export class Transfer {
       block_num: this._blockNum,
       count: this._count,
       trx_id: this._trxId,
-      memo: this._memo
-    }
+      memo: this._memo,
+    };
   }
 
   /**
    * @returns <code>false</code> if this is a single transfer or <code>true</code> if multiple transfers have been accumulated.
    */
   public isAccumulated(): boolean {
-    return !!this.count
+    return !!this.count;
   }
 
   /**
    * @returns the sender account.
    */
   get from() {
-    return this._from
+    return this._from;
   }
 
   /**
    * @returns the receiver account.
    */
   get to() {
-    return this._to
+    return this._to;
   }
 
   /**
    * @returns the token volume of this transfer.
    */
   get quantity(): number {
-    return this._quantity
+    return this._quantity;
   }
 
   /**
    * @returns the token symbol.
    */
   get symbol(): string {
-    return this._symbol
+    return this._symbol;
   }
 
   /**
    * @returns the chain.
    */
   get chain(): string {
-    return this._chain
+    return this._chain;
   }
 
   /**
    * @returns the token contract.
    */
   get contract(): string {
-    return this._contract
+    return this._contract;
   }
 
   /**
    * @returns the datetime of this transfer.
    */
   get time(): Date | undefined {
-    return this._time
+    return this._time;
   }
 
   /**
    * @returns the block number of the transaction.
    */
   get blockNum(): number | undefined {
-    return this._blockNum
+    return this._blockNum;
   }
 
   /**
    * @returns the number of accumulated transfers.
    */
   get count(): number | undefined {
-    return this._count
+    return this._count;
   }
 
   /**
    * @returns the transaction id.
    */
   get trxId(): string | undefined {
-    return this._trxId
+    return this._trxId;
   }
 
   /**
    * @returns the memo.
    */
   get memo(): string | undefined {
-    return this._memo
+    return this._memo;
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -6,10 +6,10 @@
  * preciseRound(1.00005, 4) //=> 1.0001
  */
 export function preciseRound(number: number, precision = 4) {
-  const factor = Math.pow(10, precision)
-  const tempNumber = number * factor
-  const roundedTempNumber = Math.round(tempNumber)
-  return roundedTempNumber / factor
+  const factor = Math.pow(10, precision);
+  const tempNumber = number * factor;
+  const roundedTempNumber = Math.round(tempNumber);
+  return roundedTempNumber / factor;
 }
 
 export type Amount = number;
@@ -24,17 +24,23 @@ export type Sym = string;
  * splitQuantity("45.5000 BOS") //=> [45.5, "BOS"]
  */
 export function splitQuantity(quantity: string): [Amount, Sym] {
-  if (!quantity) { throw new Error("[quantity] is required"); }
+  if (!quantity) {
+    throw new Error("[quantity] is required");
+  }
   quantity = quantity.trim();
   const [amount, sym] = quantity.split(" ");
-  if (!amount || !sym) { throw new Error("[amount] & [sym] is required"); }
+  if (!amount || !sym) {
+    throw new Error("[amount] & [sym] is required");
+  }
 
   try {
     JSON.parse(amount);
   } catch (e) {
-    throw new Error("[amount] must be a number")
+    throw new Error("[amount] must be a number");
   }
-  if (!sym.match(/[A-Z.]/)) { throw new Error("[sym] must be uppercase") }
+  if (!sym.match(/[A-Z.]/)) {
+    throw new Error("[sym] must be uppercase");
+  }
 
   return [Number(amount), sym];
 }
@@ -49,19 +55,60 @@ export function splitQuantity(quantity: string): [Amount, Sym] {
  * isAccountNameValid("eosnationftw123") //=> false
  */
 export function isAccountNameValid(name: string): boolean {
-  if (!name) { return false; }
-  return !!name.match(/^[a-z1-5.]{2,12}$/)
+  if (!name) {
+    return false;
+  }
+  return !!name.match(/^[a-z1-5.]{2,12}$/);
 }
 
 /**
  * Checks if Account name is valid by throwing Errors
  */
 export function isAccountNameValidErrors(name: string): void {
-  if (!name) { throw new Error("[name] is required") }
-  if (name.length > 12) { throw new Error("[name] must be less than 13 characters") }
-  if (name.length < 2) { throw new Error("[name] must be greater then 2 characters") }
-  if (name.match(/[06-9.]/)) { throw new Error("[name] numbers can only be 1-5") }
-  if (name.match(/[A-Z.]/)) { throw new Error("[name] letters can only lowercased") }
-  if (name.match(/[ ]/)) { throw new Error("[name] whitespaces are not valid") }
-  if (!name.match(/^[a-z1-5.]{2,12}$/)) { throw new Error("[name] is invalid") }
+  if (!name) {
+    throw new Error("[name] is required");
+  }
+  if (name.length > 12) {
+    throw new Error("[name] must be less than 13 characters");
+  }
+  if (name.length < 2) {
+    throw new Error("[name] must be greater then 2 characters");
+  }
+  if (name.match(/[06-9.]/)) {
+    throw new Error("[name] numbers can only be 1-5");
+  }
+  if (name.match(/[A-Z.]/)) {
+    throw new Error("[name] letters can only lowercased");
+  }
+  if (name.match(/[ ]/)) {
+    throw new Error("[name] whitespaces are not valid");
+  }
+  if (!name.match(/^[a-z1-5.]{2,12}$/)) {
+    throw new Error("[name] is invalid");
+  }
+}
+
+/**
+ * Convert Quantity
+ *
+ * @example
+ *
+ * convertQuantity(100010001, 'wax') //=> 1.00010001
+ * convertQuantity(1.00010001, 'wax', true) //=> 100010001
+ */
+export function convertQuantity(
+  number: number,
+  chain: string = "eos",
+  toInteger = false
+) {
+  let denom = 10000;
+  let precision = 4;
+
+  if (chain.toLowerCase() === "wax") {
+    denom = 100000000;
+    precision = 8;
+  }
+
+  if (toInteger) return Math.round(number * denom);
+  else return preciseRound(number / denom, precision);
 }
